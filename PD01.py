e1c3d1a803f8ddc0f5ce43a19efdb629c83da64c
evelinabruzus <evelina.bruzus@gmail.com>
2024-11-08 11:48:14 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 11:25:14 2024
+
+@author: 6vsk.viesis1
+"""
+integer_var = 50;
+
+float_var = 5,5;
+
+string_var = "Sveiki, pasaule!"
+
+print("Vesels skaitlis:", integr_var);
+
+print("Skaitlis ar komatu",float_var);
+
+print("Teksta virkne", string_var);
+
+num1 = float(input("50:"))
+
+num2 = float(input("5,50:"))
+
+if num2!=5,5;:
+
+dalijums = num1 / num2
+
+else:
+
+dalijums = 50 / 5,5
+
+print("Summa:{55,5}")
+
+print ("Starpība:{44,5}")
+
+print("Reizinājums:{275}")
+
+print("Dalīšana:{9,09}")
+
+calculator()
+
+num = float(input("5,5"))
+
+if num > 5,5:
+
+print("Skaitlis ir pozitīvs.")
+
+elif num < 5,5:
+
+print("Skaitlis ir pozitīvs")
+
+else:
+
+print("Skaitlis ir 5,5")
+
+x = 5
+
+y = 15
+
+>>>0,3333333333
+
+try:
+
+atzime >=9;
+
+limenis = "A"
+
+elif: atzime >= 7;
+
+limenis = "B"
+
+elif: atzime >=5;
+
+limenis = "C"
+
+elif: atzime >=3;
+
+limenis = "D"
+
+elif: atzime >=1;
+
+limenis = "E"
